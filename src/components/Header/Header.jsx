e84bf34f184bd780b46018903e6fb3f8b1591d80
Andrgoit <stakgrom@gmail.com>
2024-07-04 23:10:11 +0300

    add fonts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,11 @@
+import { Logo, Nav, SubscribeButtonSection } from "../../components";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="cont bg-orange-300 flex items-center justify-between">
+      <Logo />
+      <Nav />
+      <SubscribeButtonSection />
+    </header>
+  );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import Header from "../components/Header/Header.jsx";
+import Logo from "./Logo/Logo.jsx";
+import Nav from "./Nav/Nav.jsx";
+import SubscribeButtonSection from "./SubscribeButtonSection/SubscribeButtonSection.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 import Layout from "../components/Layout/Layout.jsx";
 
-export { Header, Footer, Layout };
+export { Header, Footer, Layout, Logo, Nav, SubscribeButtonSection };
