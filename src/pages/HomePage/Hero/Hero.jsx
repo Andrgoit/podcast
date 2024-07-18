d50e6c3d6115cb08a5c153df44211a2d89b46da7
Andrgoit <stakgrom@gmail.com>
2024-07-18 20:34:18 +0300

    upd hero

--- src/pages/HomePage/Hero/Hero.jsx
+++ src/pages/HomePage/Hero/Hero.jsx
@@ -1,11 +1,13 @@
 import Subscribe from "./Subscribe/Subscribe";
 import CoverList from "./CoverList/CoverList";
+import SupportedBy from "./SupportedBy/SupportedBy";
 
 export default function Hero() {
   return (
     <section className=" bg-[#F7EDE8] pt-16 pb-36 flex flex-col gap-32">
       <Subscribe />
       <CoverList />
+      <SupportedBy />
     </section>
   );
 }
---
+++ src/pages/HomePage/Hero/SupportedBy/SupportedBy.jsx
@@ -0,0 +1,7 @@
+export default function SupportedBy() {
+  return (
+    <div className="cont border-t border-b border-[#4D4D4D] w-full pt-11 pb-11">
+      <div></div>
+    </div>
+  );
+}
