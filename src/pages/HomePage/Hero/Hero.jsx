13d7a0f592e07427fc1632037cacba5204d2dcec
Andrgoit <stakgrom@gmail.com>
2024-07-19 15:46:21 +0300

    add component supported

--- src/pages/HomePage/Hero/Hero.jsx
+++ src/pages/HomePage/Hero/Hero.jsx
@@ -1,13 +1,20 @@
 import Subscribe from "./Subscribe/Subscribe";
 import CoverList from "./CoverList/CoverList";
 import SupportedBy from "./SupportedBy/SupportedBy";
+import nodule from "../../../assets/images/nodule.svg";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col gap-32 bg-[#F7EDE8] pb-36 pt-16">
+    <section className="relative flex flex-col gap-32 bg-[#F7EDE8] pb-36 pt-16">
       <Subscribe />
       <CoverList />
       <SupportedBy />
+      <div
+        className="absolute bottom-[-76px]"
+        style={{ right: "calc(50% - 50px)" }}
+      >
+        <img src={nodule} alt="nodule" />
+      </div>
     </section>
   );
 }
--- src/pages/HomePage/Hero/SupportedBy/SupportedBy.jsx
+++ src/pages/HomePage/Hero/SupportedBy/SupportedBy.jsx
@@ -1,7 +1,24 @@
+import y from "../../../../assets/icons/supported/Youtube.svg";
+import s from "../../../../assets/icons/supported/Spotify.svg";
+import g from "../../../../assets/icons/supported/GooglePodcast.svg";
+
 export default function SupportedBy() {
   return (
-    <div className="cont border-t border-b border-[#4D4D4D] w-full pt-11 pb-11">
-      <div></div>
+    <div className="cont w-full border-b border-t border-[#4D4D4D] pb-11 pt-11">
+      <div className="flex w-full items-center justify-between text-xl font-bold">
+        Supported by:
+        <div className="flex items-center gap-5">
+          <div className="flex w-[275px] justify-center">
+            <img src={s} alt="spotify icon" />
+          </div>
+          <div className="flex w-[275px] justify-center">
+            <img src={g} alt="googlepodcast icon" />
+          </div>
+          <div className="flex w-[275px] justify-center">
+            <img src={y} alt="youtube icon" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
